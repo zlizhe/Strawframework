c2d31b1f983e7ad10bbf76f5971ae1dddd886690
Zack Lee <zacklees@hotmail.com>
2018-11-20 17:27:08 +0800

    result init

--- Base/Result.php
+++ Base/Result.php
@@ -1,13 +1,21 @@
 <?php
+namespace Strawframework\Base;
 /**
  * User: Zack Lee
  * Date: 2018/11/18
  * Time: 9:34
  */
 
-namespace Strawframework\Base;
 
 
 class Result implements \Strawframework\Protocol\Result {
 
+    public function __construct(int $code, string $msg) {
+
+    }
+
+
+    public function setJsonP(){
+
+    }
 }
